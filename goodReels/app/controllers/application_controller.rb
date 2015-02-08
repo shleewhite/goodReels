1e3bc9dd4fe67c2eadf8fc200d6ebd5d7b95105b
coramonokandilos <coramonokandilos@Coras-MacBook-Air.local>
2015-02-07 23:59:40 -0800

    we got our first api call to work httparty at my crib holla

--- goodReels/app/controllers/application_controller.rb
+++ goodReels/app/controllers/application_controller.rb
@@ -2,7 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
   def hello
-    render text: "hello, world"
+
+  	#url = URI.parse('http://www.omdbapi.com/?t=pitch+perfect&y=&plot=short&r=json')
+
+    #render text: "hello, world"
+    #render text: url
+    render text: HTTParty.get('http://www.omdbapi.com/?t=pitch+perfect&y=&plot=short&r=json')
+
   end
+
 end
